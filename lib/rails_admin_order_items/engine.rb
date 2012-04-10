c58d112a331980f73f6fc32e4f9418210703f052
vala <gonjo@free.fr>
2012-04-10 13:19:28 +0200

    Add precompile hook so js file is precmpiled and does not throw 500 error in prode env !

--- lib/rails_admin_order_items/engine.rb
+++ lib/rails_admin_order_items/engine.rb
@@ -1,4 +1,7 @@
 module RailsAdminOrderItems
   class Engine < ::Rails::Engine
+    initializer "RailsAdminOrderItems precompile hook" do |app|
+      app.config.assets.precompile += ['rails_admin/order_items.js']
+    end
   end
 end
