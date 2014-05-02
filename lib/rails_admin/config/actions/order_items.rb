304600034b4e213d0ac62630b9c9c33af86de101
vala <gonjo@free.fr>
2014-05-02 19:04:18 +0200

    always sort new items

--- lib/rails_admin/config/actions/order_items.rb
+++ lib/rails_admin/config/actions/order_items.rb
@@ -6,7 +6,7 @@ module RailsAdmin
     module Actions
       class OrderItems < Base
         RailsAdmin::Config::Actions.register(self)
-        
+
         register_instance_option :collection do
           true
         end
@@ -22,26 +22,26 @@ module RailsAdmin
         register_instance_option :visible? do
           authorized? rescue false
         end
-        
+
         register_instance_option :template_name do
           :order_items
         end
-        
+
         register_instance_option :link_icon do
           'icon-move'
         end
-        
+
         register_instance_option :controller do
           Proc.new do
             to_model = lambda {|s| s.singularize.camelize.constantize}
+
             if request.xhr? && params[:order_model_name]
               if params[:order_action] == 'order_page' && params[:order_indexes]
-                sort = RailsAdminOrderItems::Models::ItemsOrderIndex.sort_ordered_model_items(to_model.call(params[:order_model_name]), params[:order_indexes].to_a.map(&:last)) 
+                sort = RailsAdminOrderItems::Models::ItemsOrderIndex.sort_ordered_model_items(to_model.call(params[:order_model_name]), params[:order_indexes].to_a.map(&:last))
               elsif params[:order_action] == 'place_item' && params[:item_data]
                 sort = RailsAdminOrderItems::Models::ItemsOrderIndex.place_item(to_model.call(params[:order_model_name]), params[:item_data][:id].to_i, params[:item_data][:index].to_i)
               end
             end
-
 
             @objects ||= list_entries
             @ordered_items = RailsAdminOrderItems::Models::ItemsOrderIndex.get_ordered_items(@abstract_model.to_param.camelize.constantize, 'ASC', :page => (params[:page] || 1).to_i, :per => (params[:per] || @model_config.list.items_per_page).to_i)
@@ -54,7 +54,7 @@ module RailsAdmin
           end
         end
 
-        
+
       end
     end
   end
--- lib/rails_admin_order_items/models/items_order_index.rb
+++ lib/rails_admin_order_items/models/items_order_index.rb
@@ -3,13 +3,18 @@ module RailsAdminOrderItems
     module ItemsOrderIndex
       class << self
         def get_ordered_items model, direction = 'ASC', user_options = {}
-          options = {:page => 0, :per => 40}.merge user_options
-          # Get the model name
-          model_name = model.to_s
-          # Sort the whole model entries if not every record is found
-          self.sort_model! model if model.count > ActiveRecord::Base.connection.execute("SELECT COUNT(*) FROM `items_order_indices` WHERE `model_name`='#{model_name}'").first.first
+          options = {:page => 0, :per => 40}.merge(user_options)
+
+          # # Get the model name
+          # model_name = model.to_s
+
+          # # Sort the whole model entries if not every record is found
+          # if model.count != count_items(model_name)
+            sort_model!(model)
+          # end
+
           # Find all items from the desired model ordered by their `item_index`
-          mdls = self.get_items(model, direction, options)
+          get_items(model, direction, options)
         end
 
         # Used to ensure the model has a default sorting when unordered items are found and items are retrieved with ::get_ordered_items method
@@ -19,7 +24,16 @@ module RailsAdminOrderItems
           # iteration variable to set indexes after the last existent one
           n = existing.length - 1
           # We sort the new items
-          sort_ordered_model_items model, (n <= 0 ? model.all : model.select(:id).where('id NOT IN (?)', existing.map(&:id))).map {|item| {:id => item.id, :index => (n += 1)}}
+
+          order_list = if n <= 0
+            model.all
+          else
+            model.select(:id).where(
+              'id NOT IN (?)', existing.map(&:id)
+            )
+          end.map { |item| { :id => item.id, :index => (n += 1) } }
+
+          sort_ordered_model_items(model, order_list)
         end
 
         def sort_ordered_model_items model, order_list
@@ -38,7 +52,7 @@ module RailsAdminOrderItems
             end
           end
         end
-        
+
         # Place an item from an index, used when index is filled in input field
         def place_item model, id, new_index
           # Get items list
@@ -51,13 +65,22 @@ module RailsAdminOrderItems
           n = -1
           sort_ordered_model_items model, reordered_items.map {|i| {:id => i.id, :index => (n += 1)}}
         end
-        
-        # 
+
+        #
         def get_items(model, direction, user_options = {})
           options = {:page => 0, :per => 40}.merge user_options
           model_name = model.to_s
           limit_statement = options[:page] > 0 ? " LIMIT #{options[:per] * (options[:page] - 1)},#{options[:per]}" : ''
           model.find_by_sql("SELECT `#{model.table_name}`.* FROM `#{model.table_name}` LEFT JOIN `items_order_indices` ON `#{model.table_name}`.id=`items_order_indices`.item_id WHERE `model_name`='#{model_name}' AND '#{Time.now.to_f}' > 0 ORDER BY `items_order_indices`.item_index #{direction}#{limit_statement}")
+        end
+
+        private
+
+        def count_items model_name
+          ActiveRecord::Base.connection.execute(
+            "SELECT COUNT(*) FROM `items_order_indices` " +
+              "WHERE `model_name`='#{model_name}'"
+          ).first.first
         end
       end
     end
